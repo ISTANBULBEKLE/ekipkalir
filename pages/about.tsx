cfe9bdfd1979b30d9ccab6c1d6c8ea214d6fb39b
ISTANBULBEKLE <ekipkalir@gmail.com>
2023-05-04 22:34:04 +0100

    Teck stack added

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,8 @@ import Image from 'next/image';
 
 const AboutPage:React.FC = (): JSX.Element => {
   return (
-    <div className={styles.aboutdiv}>
+    <>
+      <section className={styles.aboutsection}>
         <Image
               src="/images/ekip-kalir.png"
               width={300}
@@ -24,13 +25,35 @@ const AboutPage:React.FC = (): JSX.Element => {
           and implementation. He is a passionate, dedicated and diligent
           Software Engineer who wants to push the limits in software development.
           He has a strong background and skills in programming, project management, 
-          business analysis, team building and team management. He has a proven track
+          business analysis, team building and team management. 
+        </p>
+      </section>
+      <section className={styles.aboutsection}>
+        <h2 className={styles.abouth2}>Tech Stack</h2>
+        <p className={styles.aboutp}>
+          Ekip has a proven track
           record of analytical and problem-solving skills with robust communication
           and collaborative working. He is highly skilled in React/React Native, 
           Swift/SwiftUI with frontend and backend capabilities and Scala/Scala Play 
-          framework. 
-        </p>
-    </div>
+          framework. </p>
+        <ul className={styles.aboutul}>
+          <li className={styles.aboutli}>JavaScript</li>
+          <li className={styles.aboutli}>CSS-3</li>
+          <li className={styles.aboutli}>HTML-5</li>
+          <li className={styles.aboutli}>React.js</li>
+          <li className={styles.aboutli}>Next.js</li>
+          <li className={styles.aboutli}>Swift</li>
+          <li className={styles.aboutli}>SwiftUI</li>
+          <li className={styles.aboutli}>Scala</li>
+          <li className={styles.aboutli}>Scala/Play</li>
+          <li className={styles.aboutli}>Python</li>
+          <li className={styles.aboutli}>AWS</li>
+          <li className={styles.aboutli}>SQL</li>
+          <li className={styles.aboutli}>GraphQL</li>
+          <li className={styles.aboutli}>MongoDB</li>
+        </ul>
+      </section>
+    </>
   )
 }
 
