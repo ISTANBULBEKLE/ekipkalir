2787cea1f36ed4dcea6b074c5803aab9124263e2
ISTANBULBEKLE <ekipkalir@gmail.com>
2023-05-10 15:06:18 +0100

    homepage img added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import homeStyles from '@/styles/Home.module.css';
-
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,9 +14,15 @@ const HomePage: React.FC = (): JSX.Element => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={homeStyles.div}
-      >
+      <div className={homeStyles.container}>
+        <div className={homeStyles.imageContainer}>
+          <Image
+            src="/images/home-background.jpg"
+            layout="fill"
+            objectFit="cover"
+            alt="home-background"
+          />
+        </div>
         <h5 className={homeStyles.h5}>Welcome to my personal page.</h5>
         <p className={homeStyles.p}>
           This is my personal portfolio page. Having a system engineer background, I am coming from
