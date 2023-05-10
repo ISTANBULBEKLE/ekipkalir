0480ee8727f888bd3ba8f352ae45453af0d21943
ISTANBULBEKLE <ekipkalir@gmail.com>
2023-05-10 13:54:21 +0100

    notes.js sharing WIB

--- pages/api/notes/index.tsx
+++ pages/api/notes/index.tsx
@@ -1,5 +1,31 @@
-import {notes} from '../../../notes' 
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler (req, res) {
-    res.status(200).json(notes)
+interface Note {
+  id: number
+  content: string
+}
+
+const notes: Note[] = [
+  {
+    id: 1,
+    content: 'Note 1'
+  },
+  {
+    id: 2,
+    content: 'Note 2'
+  }
+]
+
+export default function handler (
+  req: NextApiRequest,
+  res: NextApiResponse<Note[]>
+): void {
+  /**
+   * Handle a request to retrieve notes.
+   *
+   * @param {NextApiRequest} req - The request object.
+   * @param {NextApiResponse<Note[]>} res - The response object.
+   * @returns {void}
+   */
+  res.status(200).json(notes)
 }
