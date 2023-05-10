c87c32a3fa70be9f966432331cc5ee695fd7a5be
ISTANBULBEKLE <ekipkalir@gmail.com>
2023-05-10 11:20:26 +0100

    WIB notes

--- pages/api/notes/[id].tsx
+++ pages/api/notes/[id].tsx
@@ -1,19 +1,33 @@
-import {notes} from '../../../notes' 
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(request, response) {
-    // Extract id from the query parameters
-    const id = parseInt(request.query.id);
-  
-    // Filter the notes array to get the note with the given id
-   
-    const filteredNote = notes.find((n) => n.id === id);
- 
-    // If note exists, send it in the response with a 200 status code
-    if (filteredNote) {
-      response.status(200).json(filteredNote);
-    } else {
-      // If note does not exist, send a 404 status code with an error message
-      response.status(404).json({ message: `Note with id ${id} not found` });
-    }
+interface Note {
+  id: number;
+  content: string;
+}
+
+const notes: Note[] = [{ id: 1, content: 'Note 1' }];
+
+/**
+ * Handler function for fetching a note by id.
+ * @param {NextApiRequest} request - The Next.js API request object.
+ * @param {NextApiResponse<Note | { message: string }>} response - The Next.js API response object.
+ * @returns {void}
+ */
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<Note | { message: string }>
+): void {
+  // Extract id from the query parameters
+  const id = parseInt(request.query.id as string);
+
+  // Filter the notes array to get the note with the given id
+  const filteredNote = notes.find((n) => n.id === id);
+
+  // If note exists, send it in the response with a 200 status code
+  if (filteredNote) {
+    response.status(200).json(filteredNote);
+  } else {
+    // If note does not exist, send a 404 status code with an error message
+    response.status(404).json({ message: `Note with id ${id} not found` });
   }
-
+}
